383df36afb95a0b5935ee8d88b2b949ee9bdc22a
littleastr0 <illinijonesey@gmail.com>
2018-03-30 14:02:12 -0400

    okay it works.

--- 404.php
+++ 404.php
@@ -21,7 +21,6 @@ get_header(); ?>
 				</header><!-- .page-header -->
 
 				<div class="page-content">
-					<p> :)</p>
 					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'littleastro' ); ?></p>
 
 					<?php
